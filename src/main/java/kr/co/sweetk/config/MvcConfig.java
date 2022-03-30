eb02d48cda45f866f261b7d2edb118f69aaf6864
Jumanazar <jsr1611@outlook.com>
2022-03-30 17:15:15 +0900

    updated springframework version

---
+++ src/main/java/kr/co/sweetk/config/MvcConfig.java
@@ -0,0 +1,8 @@
+package kr.co.sweetk.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter {
+}
