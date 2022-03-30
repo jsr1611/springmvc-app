f6038b3ec2dfa4cacfa35717d539dfbd151719b2
Jumanazar <jsr1611@outlook.com>
2022-03-30 17:21:46 +0900

    updated java based configs

--- src/main/java/kr/co/sweetk/MyWebInitializer.java
+++ src/main/java/kr/co/sweetk/MyWebInitializer.java
@@ -1,12 +1,13 @@
 package kr.co.sweetk;
 
+import kr.co.sweetk.config.MvcConfig;
 import kr.co.sweetk.config.SweetkConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{MvcConfig.class};
     }
 
     @Override
